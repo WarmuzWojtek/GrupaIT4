3e6b26bc16011c0b95b6e13f7b33d0c320a1c666
WarmuzWojtek <wwarmuz@gmail.com>
2021-12-07 09:54:42 +0100

    change css

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const btn = document.querySelector("button");
-const image = document.querySelector("img");
-btn.addEventListener("click", getRandomPhoto);
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("button");
+  const image = document.querySelector("img");
+  btn.addEventListener("click", getRandomPhoto);
 
-function getRandomPhoto() {
-  fetch("https://random-d.uk/api/random")
-    .then((response) => response.json())
-    .then((data) => image.setAttribute("src", data.url))
-    .catch((err) => {
-      console.error(err);
-    });
-}
+  function getRandomPhoto() {
+    fetch("https://random-d.uk/api/random")
+      .then((response) => response.json())
+      .then((data) => image.setAttribute("src", data.url))
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
